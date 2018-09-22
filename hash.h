3fadef43942a413ed472b4de4e52722d87ce8cd8
Vitor Pinheiro David <34639174+darkn00n@users.noreply.github.com>
2018-09-22 20:07:02 -0300

    Add files via upload

---
+++ hash.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "clientes.h"
+//Vitor Pinheiro David
+//Douglas Custodio de Araujo
+//Jose Lucas Alves gomes
+
+
+
+// retorna o indice onde sera inserido o registro
+// de acordo com o seu codigo
+int hash(int);
+
+// inicializa o arquivo hash de ponteiros pra listas
+void inicializa(FILE*);
+
+void insere(cliente*,FILE*,FILE*);
